500586c09c396fa1df42a4e1ef17d612b31736e1
Sahil <sahilsingh7977@gmail.com>
2022-07-12 22:55:29 +0530

    created calc obj and update method

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,47 @@
 console.log("hello world")
+
+
+class calc {
+    constructor(t1, t2) {
+            this.t1 = t1
+            this.t2 = t2
+                //     this.clear()
+        }
+        // clear() {
+        //     this.t2text = ""
+        //     this.t1.text = ""
+        //     this.oper = undefined
+        // }
+    calculate() {
+
+
+    }
+    update(text) {
+        this.t2text = this.t2text.toString() + text.toString()
+    }
+    updatedis() {
+        this.t2.innerText = this.t2text
+    }
+
+}
+
+
+
+const num = document.querySelectorAll("[data-number]")
+const oper = document.querySelectorAll("[data-operators]")
+const ac = document.querySelector("[data-ac]")
+const eq = document.querySelector("[data-equals]")
+const del = document.querySelector("[data-del]")
+const prev = document.querySelector("[data-prev]")
+const curr = document.querySelector("[data-cur]")
+const calculator = new calc(prev, curr)
+
+
+
+num.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(num.textcontent)
+        calculator.update(num.innerhtml)
+        calculator.updatedis()
+    })
+})
