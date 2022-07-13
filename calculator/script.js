b4aa8278dfd4748274851e95da0be7f636926054
Sahil <sahilsingh7977@gmail.com>
2022-07-13 12:14:19 +0530

    added logic to clear screen

--- calculator/script.js
+++ calculator/script.js
@@ -3,31 +3,32 @@ console.log("hello world")
 
 class calc {
     constructor(t1, t2) {
-            this.t1 = t1
-            this.t2 = t2
-                //     this.clear()
-        }
-        // clear() {
-        //     this.t2text = ""
-        //     this.t1.text = ""
-        //     this.oper = undefined
-        // }
+        this.t1 = t1
+        this.t2 = t2
+        this.clear()
+    }
+    clear() {
+        this.t2text = ""
+        this.t1.text = ""
+        this.oper = undefined
+    }
+    
     calculate() {
 
 
     }
     update(text) {
-        this.t2text = this.t2text.toString() + text.toString()
+        this.t2text =this.t2.textContent.toString()+ text.toString()
     }
     updatedis() {
-        this.t2.innerText = this.t2text
+        this.t2.textContent = this.t2text
     }
 
 }
 
 
 
-const num = document.querySelectorAll("[data-number]")
+const num = document.querySelectorAll(".num")
 const oper = document.querySelectorAll("[data-operators]")
 const ac = document.querySelector("[data-ac]")
 const eq = document.querySelector("[data-equals]")
@@ -40,8 +41,12 @@ const calculator = new calc(prev, curr)
 
 num.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(num.textcontent)
-        calculator.update(num.innerhtml)
+        calculator.update(button.textContent)
         calculator.updatedis()
     })
+})
+
+ac.addEventListener('click',()=>{
+    calculator.clear()
+    calculator.updatedis()
 })
